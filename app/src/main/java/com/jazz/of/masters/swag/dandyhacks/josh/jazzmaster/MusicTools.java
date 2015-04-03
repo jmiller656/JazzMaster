3361024b3407b96ac27261198b586f2c6a62e1b1
Josh Miller <jxm7374@rit.edu>
2015-04-03 19:00:56 -0400

    More stuff
    Added some music helpers

---
+++ app/src/main/java/com/jazz/of/masters/swag/dandyhacks/josh/jazzmaster/MusicTools.java
@@ -0,0 +1,25 @@
+package com.jazz.of.masters.swag.dandyhacks.josh.jazzmaster;
+
+import java.util.HashMap;
+
+/**
+ * Created by Josh on 4/3/2015.
+ */
+public class MusicTools {
+
+    String[] notes = {"A","A#","B","C","C#","D","D#","E","F","F#","G","G#","A","A#","B","C","C#","D","D#","E","F","F#","G","G#"};
+    int[] major = {4,7};
+    int[] minor = {3,7};
+    int[] dim= {3,6};
+    int[] sus2= {2,7};
+    int[] sus4= {5,7};
+    int[] aug= {4,8};
+    public String getNote()
+    {
+
+    }
+    public Chord getchord(String root, String chord)
+    {
+        //TODO
+    }
+}
